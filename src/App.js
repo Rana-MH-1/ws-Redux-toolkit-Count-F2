ae3769f4650d5c7ddb4901fd1eb301c6b4d09e0c
ACER <Rana-MH-1>
2023-01-09 18:07:00 +0100

    ws redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useSelector,useDispatch } from "react-redux";
+import {Decrement, Increment,Reset,Increment_with_value} from './Redux/CountSlice'
 
 function App() {
+  const dispatch= useDispatch()
+  const count = useSelector(state=>state.CountReducer.count)
+  const [val,setVal] = useState('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{count}</h2>
+      <button onClick={()=>dispatch(Increment())}>INCREMENT</button>
+      <button onClick={()=> dispatch(Decrement())}>Decrement</button>
+      <button onClick={()=> dispatch(Reset())}>Reset</button>
+      <input onChange={(e)=> setVal(e.target.value)} />
+      <button onClick={()=> dispatch(Increment_with_value(+val))}>Increment with value</button>
     </div>
   );
 }
